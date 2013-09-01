de6cba3a79c5c3bc4dc7a294a9abc1a60f9c40f6
PHz <i@phz.me>
2013-09-01 14:43:09 +0700

    First Form

--- js/formular1.js
+++ js/formular1.js
@@ -15,20 +15,24 @@ function quantity(){
 		
 		// calculation
 		
-		var quantity = area * tick;
-		
-		// display unit price
-		
-		 var divobj = document.getElementById('quantity');
+		var quantity = area * tick * 2;
+		var bag = quantity/20;
+		var ball = area/60*25;
+		var bbag = ball/25;
+		var primer = area/140 +1;
+				
+		// display
+			
+		var divobj = document.getElementById('quantity');
 		 divobj.style.display='block';
-		 divobj.innerHTML = "ปริมาณสีที่ใช้ "+ quantity.toFixed(2) + " กิโลกรัม";
-    
-   
+		 divobj.innerHTML = "จำนวนสีเทอร์โมพลาสติก "+ quantity.toFixed(2) + " กิโลกรัม" + "</br>" + "จำนวนถุงสีเทอร์โมพลาสติก " + bag.toFixed(0) + " ถุง (20 กิโลกรัม/ถุง)" + "</br>" + "จำนวนลูกแก้วสะท้อนแสง " + ball.toFixed(2) + " กิโลกรัม" + "</br>" 
+		 + "จำนวนถุงลูกแก้วสะท้อนแสง " + bbag.toFixed(0) + " ถุง" + "</br>"
+		 + "จำนวนน้ำยาประสานไพรเมอร์ " + primer.toFixed(0) + " ปี๊ป (13 กิโลกรัม/ปี๊บ)"
+		;	
+}
 
-		
-		
-		 
-		
-	
-	
+function hide()
+{
+    var divobj = document.getElementById('quantity');
+    divobj.style.display='none';
 }
